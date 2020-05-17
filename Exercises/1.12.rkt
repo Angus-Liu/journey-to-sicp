d2d19f4cd56a9aa676eab50e2b74c02ba55ef742
liuanguang <liuanguang@cvte.com>
2020-05-17 21:10:17 +0800

    feat: update 1.12

--- Exercises/1.12.rkt
+++ Exercises/1.12.rkt
@@ -1,3 +1,16 @@
 #lang sicp
 
 ; 帕斯卡三角形
+
+(define (pascal m n)
+  (cond ((> n m) (error "invalid param"))
+        ((= 1 n) 1)
+        ((= m n) 1)
+        (else (+ (pascal (dec m) (dec n))
+                 (pascal (dec m) n)))))
+
+(pascal 1 1)
+(pascal 2 1)
+(pascal 2 2)
+(pascal 3 2)
+(pascal 10 9)
