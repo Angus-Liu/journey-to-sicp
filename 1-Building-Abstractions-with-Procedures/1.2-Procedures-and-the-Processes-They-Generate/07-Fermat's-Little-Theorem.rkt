c8e20d330ae3f2ce333a4338b0325fc5c1687946
liuanguang <liuanguang@cvte.com>
2020-05-20 00:14:09 +0800

    feat: update 1.2-Procedures-and-the-Processes-They-Generate

--- 1-Building-Abstractions-with-Procedures/1.2-Procedures-and-the-Processes-They-Generate/07-Fermat's-Little-Theorem.rkt
+++ 1-Building-Abstractions-with-Procedures/1.2-Procedures-and-the-Processes-They-Generate/07-Fermat's-Little-Theorem.rkt
@@ -2,8 +2,23 @@
 
 ; 费马小定理：如果n是一个素数，a是小于n的任意正整数，那么a的n次方与a模n同余。
 
+(define (square n)
+  (* n n))
+
 (define (expmod base exp m)
   (cond ((= exp 0) 1)
         ((even? exp) (remainder (square (expmod base (/ exp 2) m)) m))
         (else (remainder (* base (expmod base (- exp 1) m)) m))))
-                 
+
+(define (fermat-test n)
+  (define (try-it a)
+    (= (expmod a n n) a))
+  (try-it (+ 1 (random (- n 1)))))
+
+
+(define (fast-prime? n times)
+  (cond ((= times 0) true)
+        ((fermat-test n) (fast-prime? n (- times 1)))
+        (else false)))
+
+(fast-prime? 561 50)
---
+++ Exercises/1.21.rkt
@@ -0,0 +1,20 @@
+#lang sicp
+
+(define (square n)
+  (* n n))
+
+(define (smallest-divisor n)
+  (find-divisor n 2))
+
+(define (find-divisor n test-divisor)
+  (cond ((> (square test-divisor) n) n)
+        ((divides? n test-divisor) test-divisor)
+        (else (find-divisor n (+ test-divisor 1)))))
+
+(define (divides? a b)
+  (= (remainder a b) 0))
+
+
+(smallest-divisor 199)
+(smallest-divisor 1999)
+(smallest-divisor 19999)
