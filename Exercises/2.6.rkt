665eab5a1212bd4ba7b83e77942e196d5d65edd1
liuanguang <liuanguang@cvte.com>
2020-06-06 21:19:56 +0800

    feat: update exercises 2.6

--- Exercises/2.6.rkt
+++ Exercises/2.6.rkt
@@ -20,4 +20,15 @@
 (define one (lambda (f) (lambda (x) (f x))))
 
 ; 2
+; 数值每 + 1，增加一个 f 函数的嵌套
 (define two (lambda (f) (lambda (x) (f (f x)))))
+
+; Church 加法
+(define + 
+  (lambda (m)
+    (lambda (n)
+      (lambda (f)
+        (lambda (x)
+          (m f (n f x)))))))
+
+((+ one) two)
