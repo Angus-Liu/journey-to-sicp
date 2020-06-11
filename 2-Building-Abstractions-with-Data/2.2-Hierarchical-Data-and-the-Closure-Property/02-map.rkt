49ba4e4d49fbb787c5caaa676bad3a1a3deaa10d
liuanguang <liuanguang@cvte.com>
2020-06-12 00:52:02 +0800

    feat: update 2.2-Hierarchical-Data-and-the-Closure-Property

---
+++ 2-Building-Abstractions-with-Data/2.2-Hierarchical-Data-and-the-Closure-Property/02-map.rkt
@@ -0,0 +1,9 @@
+#lang sicp
+
+(define (map proc items)
+  (if (null? items)
+      nil
+      (cons (proc (car items))
+            (map proc (cdr items)))))
+
+(map (lambda (x) (* x 10)) (list 1 2 3 4 5 6))
