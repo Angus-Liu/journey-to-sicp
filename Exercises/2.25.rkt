070eec838efc9d4d68b9023e3c554b55684f5e6a
liuanguang <liuanguang@cvte.com>
2020-06-13 02:17:16 +0800

    feat: update exercises 2.25

--- Exercises/2.25.rkt
+++ Exercises/2.25.rkt
@@ -1,1 +1,20 @@
-#lang racket
+#lang sicp
+
+; (1 3 (5 7) 9)
+(define l1 (list 1 3 (list 5 7) 9))
+(display l1)
+(newline)
+(car (cdr (car (cdr (cdr l1)))))
+
+; ((7))
+(define l2 (list (list 7)))
+(display l2)
+(newline)
+(car (car l2))
+
+
+; (1 (2 (3 (4 (5 (6 7))))))
+(define l3 (list 1 (list 2 (list 3 (list 4 (list 5 (list 6 7)))))))
+(display l3)
+(newline)
+(car (cdr (car (cdr (car (cdr (car (cdr (car (cdr (car (cdr l3))))))))))))
