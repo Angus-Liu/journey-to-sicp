563d26a8269d2cc7e34b18b7dbb0988e3eddbcbe
liuanguang <liuanguang@cvte.com>
2020-06-15 23:26:15 +0800

    feat: add exercises 2.28

--- Exercises/2.28.rkt
+++ Exercises/2.28.rkt
@@ -1,14 +1,10 @@
 #lang sicp
 
-(define (append i1 i2)
-  (cond ((null? i1) i2)
-        ((not (pair? i1)) (cons i1 i2))
-        (else (cons (car i1) (append (cdr i1) i2)))))
-
 (define (fringe items)
-  (cond ((null? items) items)
-        ((not (pair? items)) items)
-        (else (append (fringe (car items)) (fringe (cdr items))))))
+  (cond ((null? items) '()) ; 空树
+        ((not (pair? items)) (list items)) ; 叶子节点
+        (else (append (fringe (car items)) ; 处理左子树
+                      (fringe (cdr items)))))) ; 处理右子树
 
 
 (define items (list 1 (list 2 3) (list 3 4 (list 5))))
