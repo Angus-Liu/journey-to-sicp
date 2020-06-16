27ee51d78d270340e2b2d460cd997e1bb369aa08
liuanguang <liuanguang@cvte.com>
2020-06-16 22:38:51 +0800

    feat: add exercises 2.29

--- Exercises/2.29.rkt
+++ Exercises/2.29.rkt
@@ -42,3 +42,34 @@ mobile
 
 (total-weight mobile)
 
+; c)
+
+(define (torque branch)
+  (cond ((null? branch) 1)
+        ((not (pair? branch)) branch)
+        (else (* (branch-length branch) (torque (branch-structure branch))))))
+
+(define (balance tree)
+  (cond ((null? tree) #t)
+        ((not (pair? tree)) #t)
+        (else   (let ((left (left-branch tree))
+                      (right (right-branch tree)))
+                  (and (= (torque left)
+                          (torque right))
+                       (balance left)
+                       (balance right))))))
+
+
+(define balance-mobile (make-mobile (make-branch 100 (make-branch 10 10))
+                                    (make-branch 100 (make-branch 10 10))))
+
+(balance balance-mobile)
+
+; d)
+; 两处修改 cadr -> car
+; (define (right-branch mobile)
+;  (car mobile))
+
+; (define (branch-structure branch)
+;  (car branch))
+
