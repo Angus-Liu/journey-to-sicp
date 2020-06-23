bec673476a4423289594169e4b8ee5e4ada999c9
liuanguang <liuanguang@cvte.com>
2020-06-23 23:52:15 +0800

    feat: add exercises 2.36

--- Exercises/2.36.rkt
+++ Exercises/2.36.rkt
@@ -6,12 +6,18 @@
       (op (car sequence)
           (accumulate op initial (cdr sequence)))))
 
+(define (sub-seqs op seqs)
+  (if (null? seqs)
+      nil
+      (cons (op (car seqs))
+            (sub-seqs op (cdr seqs)))))
+
 (define (accumulate-n op init seqs)
   (if (null? (car seqs))
       nil
-      (cons (accumulate op init (car )
-            (accumulate-n op init ))))
+      (cons (accumulate op init (sub-seqs car seqs))
+            (accumulate-n op init (sub-seqs cdr seqs)))))
 
-(define s (list (list 1 2 3) (list 2 3 4) (list 3 4 5)))
+(define s (list (list 1 2 3) (list 20 3 4) (list 30 4 5)))
 
 (accumulate-n + 0 s)
