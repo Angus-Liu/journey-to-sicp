00f7410a2a2cfed9637f9c67154438e620a4cee6
liuanguang <liuanguang@cvte.com>
2020-06-30 00:03:48 +0800

    feat: update 02-deriv.rkt

--- 2-Building-Abstractions-with-Data/2.3-Symbolic-Data/02-deriv.rkt
+++ 2-Building-Abstractions-with-Data/2.3-Symbolic-Data/02-deriv.rkt
@@ -19,9 +19,16 @@
 (define (augend s)
   (caddr s))
 
+; 判断是否是给定数
+(define (=number? exp num)
+  (and (number? exp) (= exp num)))
+
 ; 构造和式
 (define (make-sum a1 a2)
-  (list '+ a1 a2))
+  (cond ((=number? a1 0) a2)
+        ((=number? a2 0) a1)
+        ((and (number? a1) (number? a2)) (+ a1 a2))
+        (else (list '+ a1 a2))))
 
 ; 判断是否是乘式
 (define (product? x)
@@ -37,9 +44,11 @@
 
 ; 构造乘式
 (define (make-product m1 m2)
-  (list '* m1 m2))
-
-; 判断是否是给定数
+  (cond ((or (=number? m1 0) (=number? m2 0)) 0)
+        ((=number? m1 1) m2)
+        ((=number? m2 1) m1)
+        ((and (number? m1) (number? m2)) (* m1 m2))
+        (else (list '* m1 m2))))
 
 ; 求导
 (define (deriv exp var)
@@ -60,3 +69,5 @@
 
 
 (deriv '(* (* 2 x) y) 'x)
+
+(deriv '(* x y) 'x)
