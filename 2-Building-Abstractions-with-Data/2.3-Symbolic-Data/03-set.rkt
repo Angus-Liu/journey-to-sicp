65fec4adcc0ab2e867480ecd58654102c1e2d34a
liuanguang <liuanguang@cvte.com>
2020-07-03 23:43:16 +0800

    feat: add 03-set.rkt

--- 2-Building-Abstractions-with-Data/2.3-Symbolic-Data/03-set.rkt
+++ 2-Building-Abstractions-with-Data/2.3-Symbolic-Data/03-set.rkt
@@ -1,5 +1,6 @@
 #lang sicp
 
+; 判断某一元素是否在集合中
 (define (element-of-set? x set)
   (cond ((null? set) false)
         ((equal? x (car set)) true)
@@ -11,3 +12,23 @@
 (element-of-set? 'a set)
 
 (element-of-set? 'f set)
+
+; 如果某一元素在集合中不存在，则加入
+(define (adjoin-set x set)
+  (if (element-of-set? x set)
+      set
+      (cons x set)))
+
+(adjoin-set 'f set)
+
+
+; 求两个集合的交集
+(define (intersection-set set1 set2)
+  (cond ((or (null? set1) (null? set2)) '())
+        ((element-of-set? (car set1) set2)
+         (cons (car set1) (intersection-set (cdr set1) set2)))
+        (else (intersection-set (cdr set1) set2))))
+
+(define set1 '(a b c d))
+(define set2 '(a b c e))
+(intersection-set set1 set2)
