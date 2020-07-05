e19983d407220306d34f9625fc5303a30ef8d313
liuanguang <liuanguang@cvte.com>
2020-07-05 22:16:04 +0800

    feat: update 04-Huffman

--- 2-Building-Abstractions-with-Data/2.3-Symbolic-Data/04-Huffman.rkt
+++ 2-Building-Abstractions-with-Data/2.3-Symbolic-Data/04-Huffman.rkt
@@ -1,1 +1,48 @@
-#lang racket
+#lang sicp
+
+(define (make-leaf symbol weight)
+  (list 'leaf symbol weight))
+
+(define (leaf? object)
+  (eq? (car object) 'leaf))
+
+(define (symbol-leaf x) (cadr x))
+
+(define (weight-leaf x) (caddr x))
+
+(define (make-code-tree left right)
+  (list left
+        right
+        (append (symbol-leaf left) (symbol-leaf right))
+        (+ (weight-leaf left) (weight-leaf right))))
+
+(define (left-branch tree) (car tree))
+
+(define (right-branch tree) (cadr tree))
+
+(define (symbols tree)
+  (if (leaf? tree)
+      (list (symbol-leaf tree))
+      (caddr tree)))
+
+(define (weight tree)
+  (if (leaf? tree)
+      (weight-leaf tree)
+      (cadddr tree)))
+
+(define (choose-branch bit branch)
+  (cond ((= bit 0) (left-branch branch))
+        ((= bit 1) (right-branch branch))
+        (else (error "bad bit -- choose-branch" bit))))
+
+(define (decode bits tree)
+  (define (decode-1 bits current-branch)
+    (if (null? bits)
+        '()
+        (let ((next-branch (choose-branch (car bits) current-branch)))
+          (if (leaf? next-branch)
+              (cons (symbol-leaf next-branch)
+                    (decode-1 (cdr bits) tree))
+              (decode-1 (cdr bits) next-branch)))))
+  (decode-1 bits tree))
+      
