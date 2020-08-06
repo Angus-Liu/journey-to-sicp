cea4f8023aecff111de221b493f82ef0b4b6fbac
liuanguang <liuanguang@cvte.com>
2020-08-06 08:59:47 +0800

    feat: update 3.1-Assignment-and-Local-State/01-withdraw.rkt

--- 3-Modularity-Objects-and-State/3.1-Assignment-and-Local-State/01-withdraw.rkt
+++ 3-Modularity-Objects-and-State/3.1-Assignment-and-Local-State/01-withdraw.rkt
@@ -1,10 +1,20 @@
 #lang sicp
 
-(define balance 100)
+;(define balance 100)
 
-(define (withdraw amount)
-  (if (>= balance amount)
-      (begin
-        (set! balance (- balance amount))
-        balance)
-      "Insuffcient founds"))
+;(define (withdraw amount)
+;  (if (>= balance amount)
+;      (begin
+;        (set! balance (- balance amount))
+;        balance)
+;      "Insuffcient founds"))
+
+(define new-withdraw
+  (let ((balance 100))
+    (lambda (amount)
+      (if (>= balance amount)
+          (begin
+            (set! balance (- balance amount))
+            balance)
+          "Insuffcient founds"))))
+
